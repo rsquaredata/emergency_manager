4598f299fab5b755d7a4999f901766e076f8b979
Rina <26405286+rsquaredata@users.noreply.github.com>
2026-02-01 14:50:09 +0100

    Update hospital.py

--- core/hospital.py
+++ core/hospital.py
@@ -1,18 +1,15 @@
-# core/hospital.py
+from core.resources import RessourcesService
 
 class HospitalSystem:
     def __init__(self, capacite_n: int = 10):
         self.patients = {}
-        # On passe N au service de ressources
-        self.ressources = RessourcesService(capacite_unites=capacite_n)
-        self.horloge_interne = 0
+        self.ressources = RessourcesService(n_hospital_capacity=capacite_n)
+
+    def ajouter_patient(self, patient):
+        self.patients[patient.id] = patient
 
     def calculer_indice_saturation(self) -> float:
-        """
-        IS = (Patients en SA1+SA2+SA3 + Patients en Soins Critiques) / (Capacité totale SA)
-        """
-        en_attente = sum(self.ressources.occupation_salles.values())
-        en_soins = self.ressources.patients_en_soins_critiques
-        capacite_totale_sa = sum(self.ressources.capacites_salles.values()) # 30
-        
-        return round((en_attente + en_soins) / capacite_totale_sa, 2)
+        total_sa = sum(self.ressources.occupation_sa.values())
+        cap_sa = sum(self.ressources.capacites_sa.values()) # 20
+        # IS basé sur l'occupation des 3 Salles d'Attente
+        return round(total_sa / cap_sa, 2)
